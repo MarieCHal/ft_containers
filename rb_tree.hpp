8be7fddd27e0e887105391c7d537d76a18a14de9
Marie Chalard <mariechalard@MacBook-Air-de-Marie.local>
2023-01-17 09:37:02 +0100

    node

--- rb_tree.hpp
+++ rb_tree.hpp
@@ -71,11 +71,44 @@ namespace ft
         typedef Node<value_type>*       ptr_node;      /** ptr of type node<T> */
 
         /** elements contained in a node */
-        public:
-        ptr_node                        r_child; /** pointer to right node child */
-        ptr_node                        l_child; /** pointer to left node child */
-        ptr_node                        parent;  /** pointer to parent node */
         T                               data;    /** data stored(map element) */
+        ptr_node                        parent;  /** pointer to parent node */
+        ptr_node                        r_child; /** pointer to right child node */
+        ptr_node                        l_child; /** pointer to left child node */
+        clolor                          c;
+
+         /** an empty node example used to compare nodes (used to find min) 
+          * nil being the keyword to represent emty nodes present at the end of
+          * the tree (similar to NULL)
+         */
+        static ptr_node nil;
+
+        Node() : data(), parent(NULL), r_child(NULL), l_child(NULL), c(red); /** construct an empty node */
+        Node(const Node &other) {*this = other;} /** copy constructor */
+        Node(value_type data) : data(data), parent(NULL), r_child(NULL), l_child(NULL), c(red); /** construct node with value stored */
+        Node &operator=(const Node &other) /** operator oveload */
+        {
+            this->data = other.data;
+            this->parent = other.parent
+            this->r_child = other.r_child;
+            this->l_child = other.l_child;
+            this->color = other.color;
+            return *this;
+        }
+
+        /** member functions that facilitate operation on rbtree */
+        /** @brief find the smallest value stored in the tree
+         * i.e the far most left child of the root node stops when it reaches
+         * a nil node*/
+        node_ptr minimum()
+        {
+            node_ptr end_n = this;
+            if (this == &nil)
+                return end_n;
+            while (end_n->l_child != &nil)
+                end_n = end_n->l_child;
+            return (end_n);
+        }
 
 
 
