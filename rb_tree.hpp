5efeef3a38b1e037a2c4a7eeb2bff61642a6fb67
Marie Chalard <mchalard@c1r2s3.42lausanne.ch>
2023-01-17 18:59:52 +0100

    rb_insert

--- rb_tree.hpp
+++ rb_tree.hpp
@@ -8,7 +8,7 @@
 /** SRCS:   Introduction to algorithms 3rd Edition
  *          https://www.programiz.com/dsa/red-black-tree
  * 
- * /** @brief a red balck three is a self balacing binary search three
+ * @brief a red balck three is a self balacing binary search three
  * it has the following properties:
  * - every node is coloured
  * - the root is black
@@ -22,7 +22,7 @@
  * 
  * Operations:
  *  - SEARCH --> recursive
- *  - MINIMUM
+ *  - MINIMUM 
  *  - MAXIMUM
  *  - PREDECESSOR
  *  - INSERT
@@ -61,56 +61,105 @@ namespace ft
      *  node type (T), 
      *  pointer to the key/data pair, to the left and right
      *  its color (for self balancing)
-     * 
      * */
     template <class T>
     struct Node
     {
         typedef T                       value_type; /** the value type of the stored data */
         typedef Node<value_type>        node;       /** a node of type T*/
-        typedef Node<value_type>*       ptr_node;      /** ptr of type node<T> */
+        typedef Node<value_type>*       node_ptr;      /** ptr of type node<T> */
 
         /** elements contained in a node */
         T                               data;    /** data stored(map element) */
-        ptr_node                        parent;  /** pointer to parent node */
-        ptr_node                        r_child; /** pointer to right child node */
-        ptr_node                        l_child; /** pointer to left child node */
-        clolor                          c;
+        node_ptr                        parent;  /** pointer to parent node */
+        node_ptr                        r_child; /** pointer to right child node */
+        node_ptr                        l_child; /** pointer to left child node */
+        color                           c;
 
          /** an empty node example used to compare nodes (used to find min) 
           * nil being the keyword to represent emty nodes present at the end of
           * the tree (similar to NULL)
          */
-        static ptr_node nil;
+        static node_ptr nil;
 
-        Node() : data(), parent(NULL), r_child(NULL), l_child(NULL), c(red); /** construct an empty node */
+        Node() : data(), parent(NULL), r_child(NULL), l_child(NULL), c(red) {} /** construct an empty node */
         Node(const Node &other) {*this = other;} /** copy constructor */
-        Node(value_type data) : data(data), parent(NULL), r_child(NULL), l_child(NULL), c(red); /** construct node with value stored */
+        Node(value_type data) : data(data), parent(NULL), r_child(NULL), l_child(NULL), c(red) {} /** construct node with value stored */
         Node &operator=(const Node &other) /** operator oveload */
         {
             this->data = other.data;
-            this->parent = other.parent
+            this->parent = other.parent;
             this->r_child = other.r_child;
             this->l_child = other.l_child;
             this->color = other.color;
             return *this;
         }
 
-        /** member functions that facilitate operation on rbtree */
-        /** @brief find the smallest value stored in the tree
+        /** member functions that facilitate operation and access to data on rbtree*/
+
+        /** @brief returns the node in which the smallest value of the tree is stored
          * i.e the far most left child of the root node stops when it reaches
-         * a nil node*/
-        node_ptr minimum()
+         * a nil node
+         * needed to iterate through the tree and tree operations*/
+        node_ptr    minimum()
         {
-            node_ptr end_n = this;
+            node_ptr min = this;
             if (this == &nil)
-                return end_n;
-            while (end_n->l_child != &nil)
-                end_n = end_n->l_child;
-            return (end_n);
+                return min;
+            while (min->l_child != &nil)
+                min = min->l_child;
+            return (min);
         }
 
+        /** @brief returns the node in which the bigger value of the tree is stored
+         * ie. the far most right child of the node
+         * needed to iterate through the tree and tree operations
+        */
+        node_ptr    maximum()
+        {
+            node_ptr max = this;
+            if (this == &nil)
+                return max;
+            while (max->r_child != &nil)
+                max = max->r_child;
+            return (max);
+        }
 
+        /** @brief returns the node in which the closest bigger value in the tree is stored (p.290) 
+         * example: map[4, 7, 8, 10, 6] -> succesor of 6 is 4
+         * needed to iterate through the tree
+        */
+        node_ptr    successor()
+        {
+            node_ptr r = this->r_child; 
+            if (r != nil)
+                return r->minimum(); /** if r_child return the min starting from the r_child of the node */
+            node_ptr p = this->parent;
+            while (p != nil && r == p->r_child)
+            {
+                r = p;
+                p = p->parent;
+            }
+            return p; // go up the tree until parent node if found or r isn't the right child of p
+        }
+
+        /** @brief returns the node in which the closest smaller value in the tree is stored (p.290) 
+         * example: map[4, 7, 8, 10, 6] -> succesor of 6 is 7
+         * needed to iterate through the tree
+        */
+        node_ptr    predecessor()
+        {
+            node_ptr l = this->l_child;
+            if (l != nil)
+                return l->maximum(); /** if l_child return the max starting from the l_child of the node */
+            node_ptr p = this->parent;
+            while (p != nil && l == p->l_child)
+            {
+                l = p;
+                p = p->parent;
+            }
+            return p; // go up the tree until parent node if found or r isn't the left child of p
+        }
 
     };
 
@@ -120,7 +169,6 @@ namespace ft
     class rbTree
     {
         /** @brief biderectional iterator needed to iter through the map */
-        template < class T>
         class rbIterator
         {
             public:
@@ -132,8 +180,16 @@ namespace ft
 
         };
 
+        typedef Node* node_ptr;
+        
+
         /** @brief private member of the class rbtree */
         private:
+            node_ptr _root;
+            node_ptr nil;
+
+        public:
+            rbTree() : _root(NULL), nil(NULL) {} /** constructor */
 
         public:
         /** @brief inserting a new node:
@@ -143,12 +199,75 @@ namespace ft
          * 2. Rotation
          * */
         private:
-        /** @brief right rotation **/
+            /** @brief left rotation 
+             * The arrangement on the nodes on the right is transformed into the 
+             * arrangements on the left node
+             * rotation between node x and its right child node y
+             *      x                   y
+             *         \      =>       /    
+             *            y           x 
+             * **/
+            void rb_left_rotation(node_ptr x)
+            {
+                if (x == &nil)
+                    return ;
+                node_ptr y = x->r_child;
+                x->r_child = y->l_child;
+                if (y->l_child != &nil) 
+                    y->l_child->parent = x; 
+                y->parent = x->parent;
+                if (x->parent == NULL)   
+                    this->_root = y;
+                else if (x == x->parent->l_child)
+                    x->parent->l_child = y;
+                else
+                    x->parent->r_child = y;
+                y->l_child = x;
+                x->parent = y;          
+            }
 
-        /** @brief left rotation **/
+            /** @brief right rotation 
+             * The arrangement on the nodes on the left is transformed into the 
+             * arrangements on the rigth node
+             * rotation between node x and its left child node y
+             *           x                  y
+             *          /       =>             \
+             *         y                        x
+             * **/
+            void rb_right_rotation(node_ptr x)
+            {
+                if (x == &nil)
+                    return ;
+                node_ptr y = x->l_child;
+                x->l_child = y->r_child;
+                if (y->r_child != &nil) 
+                    y->r_child->parent = x; 
+                y->parent = x->parent;
+                if (x->parent == NULL)   
+                    this->_root = y;
+                else if (x == x->parent->r_child)
+                    x->parent->r_child = y;
+                else
+                    x->parent->l_child = y;
+                y->r_child = x;
+                x->parent = y;
+            }
 
-        private:
+
         /** @brief alorithm to maintain red-black tree properties after inserting a new node **/
+
+        public:
+        /** @brief inserting an element in the red-black tree */
+        void    rb_insert(node_ptr newNode)
+        {
+            if (this->_root == &nil)
+            {
+                this->_root = newNode;
+                this->_root->c = black;
+                return ;
+            }
+
+        }
 
     };
 
