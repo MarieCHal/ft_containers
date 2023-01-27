09182ff818ab9f903fbd6f508de317aa097310a3
Marie Chalard <mariechalard@MacBook-Air-de-Marie.local>
2023-01-27 08:38:55 +0100

    rb_tree

--- rb_tree.hpp
+++ rb_tree.hpp
@@ -5,7 +5,7 @@ class rbTree;
 #include <functional>
 #include <cstddef>
 #include "iterator_traits.hpp"
-#include "rb_iterator.hpp"
+//#include "rb_iterator.hpp"
 
 /** SRCS:   Introduction to algorithms 3rd Edition
  *          https://www.programiz.com/dsa/red-black-tree
@@ -169,6 +169,89 @@ namespace ft
     template<class T, class Compare = std::less<T> >
     class rbTree
     {
+        /** @brief a bidirectional iterator to iter through the rbtree
+     * it contains a pointer to a node
+     * and a const pointer to the tree to which it belongs*/
+    //template < class T >
+    class rbBidirectionalIterator
+    {
+        public:
+            typedef T                       value_type;
+            typedef Node<value_type>        node;
+            typedef T*                      pointer;
+            typedef T&                      reference;
+            typedef typename node::node_ptr node_ptr;
+            typedef rbTree                  tree_type;
+
+        private:
+            node_ptr            _node;          /** node to which the iterator is pointing */
+            const tree_type     _tree;    /** the tree to which it belongs */
+        
+        public:
+            rbBidirectionalIterator() : _node(NULL), _tree(NULL) {}
+            rbBidirectionalIterator(node_ptr node, const tree_type *tree)
+                            : node(node), tree_type(node) {}
+            rbBidirectionalIterator(const rbBidirectionalIterator &other) 
+            {
+                this->_node = other._node;
+                this->_tree = other._tree;
+            }
+            virtual ~rbBidirectionalIterator() {}
+            // operator == ?
+            node_ptr    ptr_node() const {return this->_node;} /** return a pointer to private member _node */
+            const tree_type *tree_ptr() const {return this->_tree;} /** return a pointer to priv meber _tree */
+
+            /** @brief prefix incrementation
+             * return the iteraor after incrementation is returned
+             * if _node is one of the end node of the tree the return value */
+            rbBidirectionalIterator operator++()
+            {
+                if (this->_node == &node::nil)
+                    this->_node = this->_tree->rb_min();
+                else
+                    this->_node = this->_node->successor();
+                return *this;
+            }
+
+            /** @brief postfix incrementation
+             * return the iteraor before the incrementation is returned  */
+            rbBidirectionalIterator operator++(int)
+            {
+                rbBidirectionalIterator it_before = *this;
+                operator++();
+                return it_before;
+            }
+
+            /** @brief prefix decrementation
+             * return the iteraor after incrementation is returned */
+            rbBidirectionalIterator operator--()
+            {
+                if (this->node == &node::nil)
+                    this->_node = this->_tree->maximum();
+                else
+                    this->_node = this->_node->predecessor();
+                return *this;
+            }
+
+            /** @brief postfix incrementation
+             * return the iteraor before the decrementation is returned  */
+            rbBidirectionalIterator operator--(int)
+            {
+                rbBidirectionalIterator it_before = *this;
+                operator--();
+                return it_before;
+            }
+
+            /** @brief dereferencing the iterator (access to its value) */
+            reference operator*() {return this->_node;}
+
+            /** @brief access the value at x */
+            pointer operator->() {return &this->_node;}
+
+            /** @brief compares an iterator with this to check if they are equal 
+             * in terms of the stored node */
+    };
+
         /** @brief biderectional iterator needed to iter through the map
         class rbIterator
         {
@@ -186,7 +269,7 @@ namespace ft
         typedef std::allocator<Node<T> >                    allocator_type;
         typedef Compare                                     key_compare;
         typedef Node<T>*                                    node_ptr;
-        typedef rbBidirectionalIterator<value_type>         iterator;
+        typedef rbBidirectionalIterator         iterator;
 
         
 
